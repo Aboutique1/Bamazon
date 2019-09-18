ad9f0eb0ce12c83eb3a93313ecafc15052df98ea
AmberWebb <amberrw@yahoo.com>
2019-09-17 19:25:02 -0700

    Wrapped up the rest of my data table. I stuck with 5 this time.

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,7 @@
 insert into products
-values (1,"carol's daughters moisturizing hair creme","ethnic hair care",24.99,20);
+values (1,"Carol's Daughters Moisturizing Hair Creme","Ethnic Hair Care",24.99,20);
+values (2,"Vans Shoes","Footwear",50.00,50);
+values (3,"Empress Satin Sheets","Home Furnishings",99.99,80);
+values (4,"Pro Club White T-shirts","Men's Clothing",29.99,40);
+values (5,"Marbella Seamless Spanx","Women's Clothing",79.99,20);
 
